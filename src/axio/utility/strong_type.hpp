b042bf8cc165c8dcadc46e2167861acae93b5102
unknown-0x <254237445+unknown-0x@users.noreply.github.com>
2026-05-28 14:45:04 +0700

    Add skills

--- src/axio/utility/strong_type.hpp
+++ src/axio/utility/strong_type.hpp
@@ -1,8 +1,11 @@
 #ifndef AXIO_UTILITY_STRONG_TYPE_HPP_
 #define AXIO_UTILITY_STRONG_TYPE_HPP_
 
-#include "../base/macros.hpp"
 #include "move.hpp"
+
+#include "../base/macros.hpp"
+#include "../base/type_traits.hpp"
+#include "../string/axio_repr.hpp"
 
 namespace axio {
 template <typename T, typename Tag, template <typename> class... Skills>
@@ -18,8 +21,12 @@ struct StrongType : public Skills<StrongType<T, Tag, Skills...>>... {
   constexpr StrongType& operator=(const StrongType&) = default;
   constexpr StrongType& operator=(StrongType&&) = default;
 
-  explicit constexpr StrongType(const T& value) : value_(value) {}
-  explicit constexpr StrongType(T&& value) : value_(axio::Move(value)) {}
+  explicit constexpr StrongType(const T& value) noexcept(
+      IsNothrowCopyConstructible_V<T>)
+      : value_(value) {}
+  explicit constexpr StrongType(T&& value) noexcept(
+      IsNothrowMoveConstructible_V<T>)
+      : value_(axio::Move(value)) {}
 
   AXIO_NODISCARD constexpr T& Get() noexcept { return value_; }
   AXIO_NODISCARD constexpr const T& Get() const noexcept { return value_; }
@@ -31,6 +38,334 @@ struct StrongType : public Skills<StrongType<T, Tag, Skills...>>... {
  private:
   ValueType value_;
 };
+
+namespace detail {
+template <typename T>
+struct StrongTypeTraits;
+
+template <typename T, typename Tag, template <typename> class... Skills>
+struct StrongTypeTraits<StrongType<T, Tag, Skills...>> {
+  using ValueType = T;
+};
+
+template <typename Ptr>
+constexpr auto GetPointer(Ptr& p) -> decltype(p.operator->()) {
+  return p.operator->();
+}
+
+template <typename Element>
+constexpr auto GetPointer(Element* p) -> Element* {
+  return p;
+}
+}  // namespace detail
+
+template <typename T>
+struct Incrementable {
+  constexpr T& operator++() {
+    ++static_cast<T*>(this)->Get();
+    return *static_cast<T*>(this);
+  }
+
+  constexpr T operator++(int) {
+    T copy(*static_cast<T*>(this));
+    ++(*this);
+    return copy;
+  }
+};
+
+template <typename T>
+struct Decrementable {
+  constexpr T& operator--() {
+    --static_cast<T*>(this)->Get();
+    return *static_cast<T*>(this);
+  }
+
+  constexpr T operator--(int) {
+    T copy(*static_cast<T*>(this));
+    --(*this);
+    return copy;
+  }
+};
+
+template <typename T>
+struct BinaryAddable {
+  friend constexpr T operator+(const T& lhs, const T& rhs) {
+    return T(lhs.Get() + rhs.Get());
+  }
+
+  friend constexpr T& operator+=(T& lhs, const T& rhs) {
+    lhs.Get() += rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct BinarySubtractable {
+  friend constexpr T operator-(const T& lhs, const T& rhs) {
+    return T(lhs.Get() - rhs.Get());
+  }
+
+  friend constexpr T& operator-=(T& lhs, const T& rhs) {
+    lhs.Get() -= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct UnaryAddable {
+  constexpr T operator+() const {
+    return T(+static_cast<const T*>(this)->Get());
+  }
+};
+
+template <typename T>
+struct Negatable {
+  constexpr T operator-() const {
+    return T(-static_cast<const T*>(this)->Get());
+  }
+};
+
+template <typename T>
+struct Addable : BinaryAddable<T>, UnaryAddable<T>, Incrementable<T> {};
+
+template <typename T>
+struct Subtractable : BinarySubtractable<T>, Negatable<T>, Decrementable<T> {};
+
+template <typename T>
+struct Multiplicable {
+  friend constexpr T operator*(const T& lhs, const T& rhs) {
+    return T(lhs.Get() * rhs.Get());
+  }
+
+  friend constexpr T& operator*=(T& lhs, const T& rhs) {
+    lhs.Get() *= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct Divisible {
+  friend constexpr T operator/(const T& lhs, const T& rhs) {
+    return T(lhs.Get() / rhs.Get());
+  }
+
+  friend constexpr T& operator/=(T& lhs, const T& rhs) {
+    lhs.Get() /= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct ScalarMultiplicable {
+  using Scalar = typename detail::StrongTypeTraits<T>::ValueType;
+
+  friend constexpr T operator*(const T& lhs, const Scalar& s) {
+    return T(lhs.Get() * s);
+  }
+
+  friend constexpr T operator*(const Scalar& s, const T& rhs) {
+    return T(s * rhs.Get());
+  }
+
+  friend constexpr T& operator*=(T& lhs, const Scalar& s) {
+    lhs.Get() *= s;
+    return lhs;
+  }
+};
+
+template <typename T>
+struct ScalarDivisible {
+  using Scalar = typename detail::StrongTypeTraits<T>::ValueType;
+
+  friend constexpr T operator/(const T& lhs, const Scalar& s) {
+    return T(lhs.Get() / s);
+  }
+
+  friend constexpr T& operator/=(T& lhs, const Scalar& s) {
+    lhs.Get() /= s;
+    return lhs;
+  }
+};
+
+template <typename T>
+struct Modulable {
+  friend constexpr T operator%(const T& lhs, const T& rhs) {
+    return T(lhs.Get() % rhs.Get());
+  }
+
+  friend constexpr T& operator%=(T& lhs, const T& rhs) {
+    lhs.Get() %= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct BitwiseAndable {
+  friend constexpr T operator&(const T& lhs, const T& rhs) {
+    return T(lhs.Get() & rhs.Get());
+  }
+
+  friend constexpr T& operator&=(T& lhs, const T& rhs) {
+    lhs.Get() &= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct BitwiseOrable {
+  friend constexpr T operator|(const T& lhs, const T& rhs) {
+    return T(lhs.Get() | rhs.Get());
+  }
+
+  friend constexpr T& operator|=(T& lhs, const T& rhs) {
+    lhs.Get() |= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct BitwiseXorable {
+  friend constexpr T operator^(const T& lhs, const T& rhs) {
+    return T(lhs.Get() ^ rhs.Get());
+  }
+
+  friend constexpr T& operator^=(T& lhs, const T& rhs) {
+    lhs.Get() ^= rhs.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct BitwiseComplementable {
+  constexpr T operator~() const {
+    return T(~static_cast<const T*>(this)->Get());
+  }
+};
+
+template <typename T>
+struct BitwiseShiftable {
+  friend constexpr T operator<<(const T& lhs, const T& shift) {
+    return T(lhs.Get() << shift.Get());
+  }
+
+  friend constexpr T operator>>(const T& lhs, const T& shift) {
+    return T(lhs.Get() >> shift.Get());
+  }
+
+  friend constexpr T& operator<<=(T& lhs, const T& shift) {
+    lhs.Get() <<= shift.Get();
+    return lhs;
+  }
+
+  friend constexpr T& operator>>=(T& lhs, const T& shift) {
+    lhs.Get() >>= shift.Get();
+    return lhs;
+  }
+};
+
+template <typename T>
+struct Bitwise : BitwiseAndable<T>,
+                 BitwiseOrable<T>,
+                 BitwiseXorable<T>,
+                 BitwiseComplementable<T>,
+                 BitwiseShiftable<T> {};
+
+template <typename T>
+struct EqualityComparable {
+  friend constexpr bool operator==(const T& lhs, const T& rhs) {
+    return lhs.Get() == rhs.Get();
+  }
+
+  friend constexpr bool operator!=(const T& lhs, const T& rhs) {
+    return !(lhs == rhs);
+  }
+};
+
+template <typename T>
+struct Orderable {
+  friend constexpr bool operator<(const T& lhs, const T& rhs) {
+    return lhs.Get() < rhs.Get();
+  }
+
+  friend constexpr bool operator>(const T& lhs, const T& rhs) {
+    return rhs < lhs;
+  }
+
+  friend constexpr bool operator<=(const T& lhs, const T& rhs) {
+    return !(rhs < lhs);
+  }
+
+  friend constexpr bool operator>=(const T& lhs, const T& rhs) {
+    return !(lhs < rhs);
+  }
+};
+
+template <typename T>
+struct Comparable : EqualityComparable<T>, Orderable<T> {};
+
+template <typename T>
+struct Dereferenceable {
+  using U = typename detail::StrongTypeTraits<T>::ValueType;
+
+  constexpr U& operator*() & { return static_cast<T*>(this)->Get(); }
+
+  constexpr U const& operator*() const& {
+    return static_cast<const T*>(this)->Get();
+  }
+};
+
+template <typename T>
+struct ImplicitBool {
+  constexpr explicit operator bool() const {
+    return static_cast<bool>(static_cast<const T*>(this)->Get());
+  }
+
+  constexpr bool operator!() const {
+    return !static_cast<bool>(*static_cast<const T*>(this));
+  }
+};
+
+template <typename T>
+struct AxioReprable {
+  template <typename Output>
+  friend void AxioRepr(Output& output, const T& value) {
+    AxioRepr(output, value.Get());
+  }
+};
+
+template <typename T>
+struct Arithmetic : Addable<T>,
+                    Subtractable<T>,
+                    Divisible<T>,
+                    Multiplicable<T>,
+                    Modulable<T>,
+                    Bitwise<T>,
+                    Comparable<T>,
+                    AxioReprable<T> {};
+
+template <typename T>
+struct PointerLike : Comparable<T>, ImplicitBool<T> {
+  using U = typename detail::StrongTypeTraits<T>::ValueType;
+
+  constexpr decltype(auto) operator*() & {
+    return *static_cast<T*>(this)->Get();
+  }
+
+  constexpr decltype(auto) operator*() const& {
+    return *static_cast<const T*>(this)->Get();
+  }
+
+  constexpr auto operator->() {
+    return detail::GetPointer(static_cast<T*>(this)->Get());
+  }
+
+  constexpr const auto operator->() const {
+    return detail::GetPointer(static_cast<const T*>(this)->Get());
+  }
+};
+
+template <typename T>
+struct Flag : Bitwise<T>, Comparable<T>, AxioReprable<T> {};
 }  // namespace axio
 
 #endif
--- test/utility/strong_type.cpp
+++ test/utility/strong_type.cpp
@@ -1,6 +1,7 @@
 #include <simpletest/simpletest.hpp>
 
 #include <axio/base/type_traits.hpp>
+#include <axio/string/string_utils.hpp>
 #include <axio/utility/strong_type.hpp>
 
 #include <memory>
@@ -157,4 +158,372 @@ TEST_CASE(StrongType, MoveOnlyType) {
   MoveOnly moved_mo(axio::Move(mo));
   CHECK_EQ(*moved_mo.Get(), 99);
   CHECK_EQ(mo.Get(), nullptr);
+}
+
+// Skills
+
+using Pointer =
+    axio::StrongType<std::unique_ptr<int>, struct PtrTag, axio::PointerLike>;
+using Integer = axio::StrongType<int, struct IntegerTag, axio::Arithmetic>;
+using Temperature = axio::StrongType<float,
+                                     struct TemperatureTag,
+                                     axio::Addable,
+                                     axio::Subtractable,
+                                     axio::ScalarMultiplicable,
+                                     axio::ScalarDivisible,
+                                     axio::Comparable>;
+
+namespace test {
+constexpr Integer a(10), b(2), c(2);
+static_assert((a + b) == Integer(12), "");
+static_assert((a - b) == Integer(8), "");
+static_assert((a * b) == Integer(20), "");
+static_assert((a % b) == Integer(0), "");
+static_assert((a / b) == Integer(5), "");
+
+static_assert(a != b, "");
+static_assert(b == c, "");
+static_assert(a > b, "");
+static_assert(a >= b, "");
+static_assert(b >= c, "");
+static_assert(b < a, "");
+static_assert(b <= a, "");
+static_assert(b <= c, "");
+
+constexpr Integer negative_a(-a);
+static_assert(negative_a.Get() == -a.Get(), "");
+
+constexpr int value_a = static_cast<int>(a);
+static_assert(value_a == a.Get(), "");
+
+constexpr bool Test() {
+  bool ret = true;
+
+  Integer x(10), y(2), z(3);
+  z += x;
+  ret = ret && (z == Integer(13));
+  z -= y;
+  ret = ret && (z == Integer(11));
+
+  z *= y;
+  ret = ret && (z == Integer(22));
+
+  z /= y;
+  ret = ret && (z == Integer(11));
+
+  z %= y;
+  ret = ret && (z == Integer(1));
+
+  ret = ret && (++z == Integer(2));
+  ret = ret && (z++ == Integer(2));
+  ret = ret && (z == Integer(3));
+  ret = ret && (--z == Integer(2));
+  ret = ret && (z-- == Integer(2));
+  ret = ret && (z == Integer(1));
+
+  return ret;
+}
+static_assert(Test(), "");
+}  // namespace test
+
+TEST_CASE(StrongType_Skills, Integer) {
+  Integer a(10), b(2);
+  CHECK_EQ(a + b, Integer(12));
+  CHECK_EQ(a - b, Integer(8));
+  CHECK_EQ(a * b, Integer(20));
+  CHECK_EQ(a % b, Integer(0));
+  CHECK_EQ(a / b, Integer(5));
+
+  Integer c(3);
+  c += a;
+  CHECK_EQ(c, Integer(13));
+  c -= b;
+  CHECK_EQ(c, Integer(11));
+  c *= b;
+  CHECK_EQ(c, Integer(22));
+  c /= b;
+  CHECK_EQ(c, Integer(11));
+  c %= b;
+  CHECK_EQ(c, Integer(1));
+
+  CHECK_EQ(++c, Integer(2));
+  CHECK_EQ(c++, Integer(2));
+  CHECK_EQ(c, Integer(3));
+  CHECK_EQ(--c, Integer(2));
+  CHECK_EQ(c--, Integer(2));
+  CHECK_EQ(c, Integer(1));
+
+  a = Integer(12);
+  b = Integer(4);
+  c = Integer(4);
+  CHECK_TRUE(a != b);
+  CHECK_TRUE(b == c);
+  CHECK_TRUE(a > b);
+  CHECK_TRUE(a >= b);
+  CHECK_TRUE(b >= c);
+  CHECK_TRUE(b < a);
+  CHECK_TRUE(b <= a);
+  CHECK_TRUE(b <= c);
+
+  // AxioReprable
+  auto str = axio::StringJoinValues(", ", a, b, c);
+  CHECK_EQ(str, "12, 4, 4");
+
+  a = -a;
+  b = +b;
+  CHECK_EQ(a, Integer(-12));
+  CHECK_EQ(b, Integer(4));
+}
+
+namespace test_bitwise {
+constexpr Integer a(0b1100);
+constexpr Integer b(0b1010);
+
+static_assert((a & b) == Integer(0b1000), "");
+static_assert((a | b) == Integer(0b1110), "");
+static_assert((a ^ b) == Integer(0b0110), "");
+static_assert(~a == Integer(~0b1100), "");
+
+static_assert((Integer(1) << Integer(3)) == Integer(8), "");
+static_assert((Integer(16) >> Integer(2)) == Integer(4), "");
+}  // namespace test_bitwise
+
+TEST_CASE(StrongType_Skills, Integer_Bitwise) {
+  Integer a(0b1100);
+  Integer b(0b1010);
+
+  CHECK_EQ((a & b), Integer(0b1000));
+  CHECK_EQ((a | b), Integer(0b1110));
+  CHECK_EQ((a ^ b), Integer(0b0110));
+
+  CHECK_EQ(~a, Integer(~0b1100));
+
+  Integer c(a);
+  c &= b;
+  CHECK_EQ(c, Integer(0b1000));
+
+  c = a;
+  c |= b;
+  CHECK_EQ(c, Integer(0b1110));
+
+  c = a;
+  c ^= b;
+  CHECK_EQ(c, Integer(0b0110));
+
+  a = Integer(1);
+  CHECK_EQ((a << Integer(3)), Integer(8));
+  b = Integer(16);
+  CHECK_EQ((b >> Integer(2)), Integer(4));
+
+  a <<= Integer(3);
+  b >>= Integer(2);
+  CHECK_EQ(a, Integer(8));
+  CHECK_EQ(b, Integer(4));
+}
+
+namespace test_scalar_muldiv {
+constexpr Temperature c(100.0f);
+constexpr Temperature f = c * 1.8f + Temperature(32.0f);
+static_assert(f == Temperature(212.f), "");
+
+constexpr bool Test() {
+  Temperature t(f);
+  t /= 2.0f;
+  return t.Get() == 106.0f;
+}
+static_assert(Test(), "");
+}  // namespace test_scalar_muldiv
+
+TEST_CASE(StrongType_Skills, Scalar_MulDiv) {
+  Temperature c(100.0f);
+  Temperature f = c * 1.8f + Temperature(32.0f);
+  CHECK_NEAR(f.Get(), 212.f, 0.00001f);
+
+  f /= 2.0f;
+  CHECK_NEAR(f.Get(), 106.f, 0.00001f);
+}
+
+TEST_CASE(StrongType_Skills, Dereferenceable) {
+  using StrongInt = StrongType<int, struct StrongIntTag, axio::Dereferenceable>;
+
+  StrongInt si{1};
+  const StrongInt csi{1};
+
+  int& value = *si;
+  // int& value = *csi; // must not compile
+  const int& cvalue = *csi;
+
+  CHECK_EQ(value, 1);
+  CHECK_EQ(cvalue, 1);
+
+  value = 10;
+  CHECK_EQ(si.Get(), 10);
+
+  static constexpr StrongInt constexpr_si(10);
+  static_assert(*constexpr_si == 10, "");
+}
+
+TEST_CASE(StrongType_Skills, PointerLike) {
+  {
+    Pointer p(std::make_unique<int>(42));
+    CHECK_EQ(*p, 42)
+    CHECK_TRUE(bool(p));  // ImplicitBool
+
+    Pointer moved(axio::Move(p));
+    CHECK_EQ(*moved, 42);
+    CHECK_TRUE(bool(moved));
+    CHECK_FALSE(bool(p));
+  }
+  {
+    using StringPtr = axio::StrongType<std::unique_ptr<std::string>,
+                                       struct StringPtrTag, axio::PointerLike>;
+    StringPtr ptr(std::make_unique<std::string>("hello"));
+    CHECK_EQ(ptr->size(), 5u);
+    CHECK_EQ(ptr->at(0), 'h');
+
+    (*ptr).append(" world");
+    CHECK_EQ(*ptr, "hello world");
+  }
+  {
+    Pointer valid(std::make_unique<int>(1));
+    Pointer empty(nullptr);
+
+    CHECK_FALSE(!valid);
+    CHECK_TRUE(!empty);
+  }
+  {
+    using FooPtr = StrongType<Foo*, struct FooPtrTag, axio::PointerLike>;
+    using CFooPtr =
+        StrongType<const Foo*, struct CFooPtrTag, axio::PointerLike>;
+
+    Foo f(100);
+    const Foo cf(200);
+
+    FooPtr fp(&f);
+    // FooPtr fp(&cf); // must not compile
+    CFooPtr cfp(&cf);
+
+    CHECK_EQ(fp->value, 100);
+    CHECK_EQ(cfp->value, 200);
+
+    fp->value = 50;
+    CHECK_EQ(fp->value, 50);
+
+    CHECK_EQ((*cfp).value, 200);
+    // cfp->value = 100; // must not compile
+
+    Foo& fref = *fp;
+    fref.value = 123;
+    CHECK_EQ(fp->value, 123);
+
+    static constexpr Foo ce_f(100);
+    static constexpr CFooPtr ce_cfp(&ce_f);
+    static_assert(ce_cfp->value == 100, "");
+  }
+}
+
+enum class FilePermission : std::uint8_t {
+  None = 0,
+  Read = 1 << 0,
+  Write = 1 << 1,
+  Execute = 1 << 2
+};
+
+AXIO_DEFINE_ENUM_BITWISE(FilePermission);
+
+template <typename Output>
+void AxioRepr(Output& output, FilePermission fp) {
+  const char* result = "<unknown>";
+  switch (fp) {
+    case FilePermission::None:
+      result = "FP::None";
+      break;
+    case FilePermission::Read:
+      result = "FP::Read";
+      break;
+    case FilePermission::Write:
+      result = "FP::Write";
+      break;
+    case FilePermission::Execute:
+      result = "FP::Execute";
+      break;
+  }
+  output.Append(result);
+}
+
+using Permissions =
+    StrongType<FilePermission, struct FilePermissionsTag, axio::Flag>;
+
+namespace test_flag {
+constexpr bool BitwiseOr() {
+  Permissions read(FilePermission::Read);
+  Permissions write(FilePermission::Write);
+  return (read | write).Get() == (FilePermission::Read | FilePermission::Write);
+}
+
+constexpr bool BitwiseAnd() {
+  Permissions value(FilePermission::Read | FilePermission::Write);
+  Permissions read(FilePermission::Read);
+  return (value & read).Get() == (FilePermission::Read);
+}
+
+constexpr bool BitwiseXor() {
+  Permissions value(FilePermission::Read | FilePermission::Write);
+  Permissions write(FilePermission::Write);
+  return (value ^ write).Get() == (FilePermission::Read);
+}
+
+static_assert(BitwiseOr(), "");
+static_assert(BitwiseAnd(), "");
+static_assert(BitwiseXor(), "");
+static_assert((~Permissions(FilePermission::Read)).Get() ==
+                  ~FilePermission::Read,
+              "");
+}  // namespace test_flag
+
+TEST_CASE(StrongType_FlagTest, BitwiseOr) {
+  Permissions read(FilePermission::Read);
+  Permissions write(FilePermission::Write);
+
+  auto result = read | write;
+  CHECK_EQ(result.Get(), (FilePermission::Read | FilePermission::Write));
+}
+
+TEST_CASE(StrongType_FlagTest, BitwiseAnd) {
+  Permissions value(FilePermission::Read | FilePermission::Write);
+  Permissions read(FilePermission::Read);
+
+  auto result = value & read;
+  CHECK_EQ(result.Get(), FilePermission::Read);
+}
+
+TEST_CASE(StrongType_FlagTest, BitwiseXor) {
+  Permissions value(FilePermission::Read | FilePermission::Write);
+  Permissions write(FilePermission::Write);
+
+  auto result = value ^ write;
+  CHECK_EQ(result.Get(), FilePermission::Read);
+}
+
+TEST_CASE(StrongType_FlagTest, BitwiseNot) {
+  Permissions read(FilePermission::Read);
+  auto result = ~read;
+  CHECK_EQ(result.Get(), ~FilePermission::Read);
+}
+
+TEST_CASE(StrongType_FlagTest, Comparable) {
+  Permissions a(FilePermission::Read);
+  Permissions b(FilePermission::Read);
+  Permissions c(FilePermission::Write);
+
+  CHECK_EQ(a, b);
+  CHECK_NE(a, c);
+}
+
+TEST_CASE(StrongType_FlagTest, Repr) {
+  Permissions a(FilePermission::Read);
+  Permissions b(FilePermission::Write);
+  Permissions c(FilePermission::Execute);
+  auto str = axio::StringJoinValues(" | ", a, b, c);
+  CHECK_EQ(str, "FP::Read | FP::Write | FP::Execute");
 }
